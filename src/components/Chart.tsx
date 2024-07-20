39f42c63aae601a3e0615957e8b07596c2abade6
salman4ll <sfarisi0101@gmail.com>
2024-07-20 10:31:58 +0700

    revisi terakhir terakhir bismillah, capenya oy ngurusin bug

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -48,10 +48,11 @@ const Chart: React.FC = () => {
     ssr: false,
   });
 
-  const options: ApexOptions = {
-    chart: {
-      type: 'line', // Set chart type to 'line'
-    },
+  const options: ApexOptions = {  
+    title: {
+      text: `Incident Data Overview ${year}`,
+      align: "left",
+    },  
     xaxis: {
       type: "category",
       categories: month,
@@ -63,32 +64,18 @@ const Chart: React.FC = () => {
       },
     },
     yaxis: {
-      max: 10, // Set the y-axis maximum value to 10
-    },
+      max: 5
+    }
   };
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white px-5 pb-5 pt-7.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-8">
-      <div className="flex flex-wrap items-start justify-between gap-3 sm:flex-nowrap">
-        <div className="flex w-full flex-wrap gap-3 sm:gap-5">
-          <div className="flex min-w-47.5">
-            <span className="mr-2 mt-1 flex h-4 w-full max-w-4 items-center justify-center rounded-full border border-primary">
-              <span className="block h-2.5 w-full max-w-2.5 rounded-full bg-primary"></span>
-            </span>
-            <div className="w-full">
-              <p className="font-semibold text-primary">Total Incidents</p>
-              <p className="text-sm font-medium">Year {year}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <div>
-        <div id="chartOne" className="-ml-5">
+        <div id="chartOne" className="-ml-5 p-5">
           <ReactApexChart
             options={options}
             series={series}
-            type="line" // Set chart type to 'line'
+            type="area"
             height={350}
             width={"100%"}
           />
